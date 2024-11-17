c0c9f1c96bcc3c7fe8a9798cb93f82c8be387ab0
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-17 01:01:44 -0500

    Add `HostController.Transport.USB`

--- Sources/BTStack/Transport.swift
+++ Sources/BTStack/Transport.swift
@@ -9,12 +9,29 @@ import CBTStack
 
 public extension HostController {
     
-    struct Transport {
+    enum Transport {
         
-        internal let pointer: UnsafePointer<hci_transport_t>
-        
-        internal init(_ pointer: UnsafePointer<hci_transport_t>) {
-            self.pointer = pointer
+        case usb(USB = USB())
+    }
+}
+
+public extension HostController.Transport {
+    
+    static var `default`: HostController.Transport {
+        #if os(macOS) || os(Linux)
+        return .usb(USB())
+        #else
+        return .usb(USB())
+        #endif
+    }
+}
+
+internal extension HostController.Transport {
+    
+    var pointer: UnsafePointer<hci_transport_t> {
+        switch self {
+        case let .usb(transport):
+            return transport.pointer
         }
     }
 }
@@ -22,5 +39,25 @@ public extension HostController {
 public extension HostController.Transport {
     
     /// USB Transport
-    static var usb: HostController.Transport { .init(hci_transport_usb_instance()) }
+    struct USB {
+        
+        internal let pointer: UnsafePointer<hci_transport_t>
+        
+        internal init(_ pointer: UnsafePointer<hci_transport_t>) {
+            self.pointer = pointer
+        }
+        
+        public init() {
+            self.init(hci_transport_usb_instance()) // singleton
+        }
+        
+        public func setPath(_ path: [UInt8]) {
+            var path = path
+            hci_transport_usb_set_path(Int32(path.count), &path)
+        }
+        
+        public func addDevice(vendor: UInt16, product: UInt16) {
+            hci_transport_usb_add_device(vendor, product)
+        }
+    }
 }
